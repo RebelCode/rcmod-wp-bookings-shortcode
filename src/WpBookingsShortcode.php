5aa57be70c6a83b47e53b1633a31176312ba6e03
Roman Lytvynenko <litvinenko95@gmail.com>
2018-06-28 12:17:07 +0300

    used events for rendering client application

--- src/WpBookingsShortcode.php
+++ src/WpBookingsShortcode.php
@@ -9,20 +9,35 @@ use Psr\EventManager\EventManagerInterface;
 use RebelCode\Modular\Module\AbstractBaseModule;
 use Dhii\Util\String\StringableInterface as Stringable;
 
+/**
+ * Handler for bookings shortcode that will insert client application
+ * on page for booking appointments.
+ *
+ * @since [*next-version*]
+ */
 class WpBookingsShortcode extends AbstractBaseModule
 {
+    /**
+     * The name of shortcode tag.
+     *
+     * @since [*next-version*]
+     *
+     * @var string
+     */
+    protected $shortcodeTag;
+
     /**
      * Constructor.
      *
      * @since [*next-version*]
      *
-     * @param string|Stringable $key The module key.
-     * @param string[]|Stringable[] $dependencies The module  dependencies.
-     * @param ContainerFactoryInterface $configFactory The config factory.
-     * @param ContainerFactoryInterface $containerFactory The container factory.
+     * @param string|Stringable         $key                  The module key.
+     * @param string[]|Stringable[]     $dependencies         The module  dependencies.
+     * @param ContainerFactoryInterface $configFactory        The config factory.
+     * @param ContainerFactoryInterface $containerFactory     The container factory.
      * @param ContainerFactoryInterface $compContainerFactory The composite container factory.
-     * @param EventManagerInterface $eventManager The event manager.
-     * @param EventFactoryInterface $eventFactory The event factory.
+     * @param EventManagerInterface     $eventManager         The event manager.
+     * @param EventFactoryInterface     $eventFactory         The event factory.
      */
     public function __construct(
         $key,
@@ -32,8 +47,7 @@ class WpBookingsShortcode extends AbstractBaseModule
         $compContainerFactory,
         $eventManager,
         $eventFactory
-    )
-    {
+    ) {
         $this->_initModule($key, $dependencies, $configFactory, $containerFactory, $compContainerFactory);
         $this->_initModuleEvents($eventManager, $eventFactory);
     }
@@ -55,17 +69,41 @@ class WpBookingsShortcode extends AbstractBaseModule
      */
     public function run(ContainerInterface $c = null)
     {
-        add_shortcode($c->get('shortcode_tag'), function ($attrs) use ($c) {
+        $this->shortcodeTag = $c->get('shortcode_tag');
+
+        add_shortcode($this->shortcodeTag, function ($attrs) {
             $attrs = $attrs ? $attrs : [];
-            return $c->get('wp_bookings_front_ui')->render($attrs);
+
+            return $this->_trigger('eddbk_wizard_main_component', $attrs)->getParam('content');
         });
 
-        $this->eventManager->attach('wp_enqueue_scripts', function () use ($c) {
-            if (!is_a(get_post(), 'WP_Post') || !has_shortcode(get_post()->post_content, $c->get('shortcode_tag'))) {
+        $this->_attach('wp_enqueue_scripts', function () {
+            if (!$this->_shouldRenderShortcodeContent()) {
                 return;
             }
+            $this->_trigger('eddbk_wizard_enqueue_assets');
+            $this->_trigger('eddbk_wizard_enqueue_app_state');
+        }, 999);
 
-            $c->get('wp_bookings_front_ui')->enqueueAssets($c);
+        $this->_attach('wp_footer', function () {
+            if (!$this->_shouldRenderShortcodeContent()) {
+                return;
+            }
+            echo $this->_trigger('eddbk_wizard_components_templates', [
+                'content' => '',
+            ])->getParam('content');
         });
+    }
+
+    /**
+     * Check that shortcode's content should be rendered.
+     *
+     * @since [*next-version*]
+     *
+     * @return bool Is shortcode's content should be rendered.
+     */
+    protected function _shouldRenderShortcodeContent()
+    {
+        return is_a(get_post(), 'WP_Post') && has_shortcode(get_post()->post_content, $this->shortcodeTag);
     }
 }
